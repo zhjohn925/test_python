04e34bc5d8ddde0d7b41b47fb19fc0a7e0852bfc
John Zhuang <zhjohn925@gmail.com>
2018-05-08 22:34:10 -0700

    update

--- makinggames/memorypuzzle.py
+++ makinggames/memorypuzzle.py
@@ -12,7 +12,7 @@ WINDOWHEIGHT = 480 # size of windows' height in pixels
 REVEALSPEED = 8 # speed boxes' sliding reveals and covers
 BOXSIZE = 40 # size of box height & width in pixels
 GAPSIZE = 10 # size of gap between boxes in pixels
-BOARDWIDTH = 10 # number of columns of icons
+BOARDWIDTH = 6 #10 # number of columns of icons
 BOARDHEIGHT = 7 # number of rows of icons
 assert (BOARDWIDTH * BOARDHEIGHT) % 2 == 0, 'Board needs to have an even number of boxes for pairs of matches.'
 XMARGIN = int((WINDOWWIDTH - (BOARDWIDTH * (BOXSIZE + GAPSIZE))) / 2)
--- makinggames/slidepuzzle.py
+++ makinggames/slidepuzzle.py
@@ -3,6 +3,19 @@ from pygame.locals import *
 
 # https://www.pygame.org/docs/
 
+BOARDWIDTH = 4
+BOARDHEIGHT = 4
+
+UP = 'up'
+DOWN = 'down'
+LEFT = 'left'
+RIGHT = 'right'
+
+def main():
+   global FPSCLOCK
+   pygame.init()
+
+
 def terminate():
    pygame.quit()
    sys.exit()
@@ -20,11 +33,41 @@ def checkForQuit():
 #for example, if BOARDWIDTH=BOARDHEIGHT=3,
 #then board=[[1,4,7],[2,5,8],[3,6,None]]
 def getStartingBoard():
-   counter = 1
-   board = []
-   for x in range(BOARDWIDTH):
-      column = []
-      for y in range(BOARDHEIGHT):
-         column.append(counter)
-         counter += BOARDWIDTH
-      board.append(column)
+    board = []
+    for x in range(BOARDWIDTH):
+       column = []
+       counter = x+1
+       for y in range(BOARDHEIGHT):
+          column.append(counter)
+          counter += BOARDWIDTH
+       board.append(column)
+    #the last one as blank   
+    board[BOARDWIDTH-1][BOARDHEIGHT-1] = None
+    return board
+
+#return which tile is blank (None)
+def getBlankPosition(board):
+	for x in range(BOARDWIDTH):
+		for y in range(BOARDHEIGHT):
+			if (board[x][y]==None):
+				return(x,y)
+
+def makeMove(board, move):
+	x, y = getBlankPosition(board)
+	if move==UP:
+		board[x][y] = board[x][y+1]
+		board[x][y+1] = None
+	elif move==DOWN:
+		board[x][y] = board[x][y-1]
+		board[x][y-1] = None
+	elif move==LEFT:
+		board[x][y] = board[x+1][y]
+		board[x+1][y] = None
+	elif move==RIGHT:
+		board[x][y] = board[x-1][y]
+		board[x-1][y] = None					 	
+
+
+
+if __name__ == '__main__':
+	main()
