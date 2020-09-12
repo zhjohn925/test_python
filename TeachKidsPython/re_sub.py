b8ade929489fdf8e5ff3b1f2910fd07f78bf8dfa
zhjohn925 <zhjohn925@gmail.com>
2020-09-12 15:52:52 -0700

    Create re_sub.py

---
+++ TeachKidsPython/re_sub.py
@@ -0,0 +1,15 @@
+# Program to remove all whitespaces
+import re
+
+# multiline string
+string = 'abc 12\
+de 23 \n f45 6'
+
+# matches all whitespace characters
+pattern = '\s+'
+
+# empty string
+replace = ''
+
+new_string = re.sub(pattern, replace, string) 
+print(new_string)
