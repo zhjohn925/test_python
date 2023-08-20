2a99c5da31d31f12b9f8d27157ada0c4317e9be1
zhjohn925 <zhjohn925@gmail.com>
2023-08-19 21:07:38 -0700

    Update and rename 001_notes.txt to 001_notes.py

---
+++ dudu/001_notes.py
@@ -0,0 +1,16 @@
+# data types:
+int, string, boolean, float
+list, set, dict (container)
+
+
+boolean: True, False
+True: 1,2,..., "a", [1], (1,2), {"key":1, "abe":2}
+False: 0,      "",  [],  (,),   {} , None
+
+container is iterable.  for ... loop,  while ... loop, 
+
+d = {}
+dir(d)
+type(d)
+
+
